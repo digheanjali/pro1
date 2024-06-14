3e06da85156978217b77b237783b3abbc357367e
pawaseanjali <rahuldighe711@gmail.com>
2024-06-14 02:04:46 -0700

    added

--- cypress/e2e/10-Step-Defination/AE22cy.js
+++ cypress/e2e/10-Step-Defination/AE22cy.js
@@ -1,11 +1,15 @@
 ///<reference types='cypress' />
-import page from"../9-Page/AE2"
-import data2 from"../../fixtures/AE22Payload"
+import page from "../9-Page/AE2"
+import data2 from "../../fixtures/AE22Payload"
 
-describe('verify Automation Exercise page',function(){
+describe('verify Automation Exercise page', function () {
     let title = undefined
 
-    it('verify login page',function(){
-        
+    let pg = new page
+
+    it('verify login page', function () {
+
+
+
     })
 })
